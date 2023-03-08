fd4bbf56ac90af4cbc4b384d1115cdf42abb2b09
Takeyuki UEDA <ueda@latelierdueda.com>
2023-03-08 12:57:55 +0900

    add test

---
+++ test/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <!-- Navigation bar -->
+    <Navbar :links="links"/>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import {Navbar} from 'vuetify-nav2'
+import { mdiHome, /*mdiArrowLeft*/} from "@mdi/js";
+
+export default {
+  components: { Navbar},
+  data: () => ({
+    links: [
+      { icon: mdiHome, text: 'Home', route: '/'},
+      { icon: '', text: 'About', route: '/abuot'},
+    ]
+  }),
+}
+</script>
